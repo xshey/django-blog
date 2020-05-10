1c7cbe8377bdff4447e24e5c1ed7ba90b6b55ae0
tshepho boya <tshephoboya@aol.com>
2020-05-10 11:40:21 +0200

    Apply CSS

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
@@ -122,5 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    STATIC_URL,
+    STATIC_DIR,
 ]
